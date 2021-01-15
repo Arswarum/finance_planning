1902d550def6bb440e2081157bc564140d7753cd
arswarum <bajda.dmytro@gmail.com>
2021-01-15 12:36:22 +0100

    draft but working login and registration pages add

---
+++ finance_planning/users/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from users.forms import RegisterForm
+
+# Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/bugget') # maybe to change (to login page)
+    else:
+        form = RegisterForm()
+
+    return render(request, 'users/register.html', {'form': form})
