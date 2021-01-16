2fc8874046739ad7a677f2ab68cb54a0d93f83dc
arswarum <bajda.dmytro@gmail.com>
2021-01-16 10:20:06 +0100

    templates created but not ended

--- finance_planning/users/views.py
+++ finance_planning/users/views.py
@@ -11,7 +11,7 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect('/bugget') # maybe to change (to login page)
+        return redirect('login') # maybe to change (to login page)
     else:
         form = RegisterForm()
 
