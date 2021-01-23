11b1e23e35839d39fe409514b40d5b7a7f8cdcb0
arswarum <bajda.dmytro@gmail.com>
2021-01-23 13:20:58 +0100

    all works

--- finance_planning/budget/forms.py
+++ finance_planning/budget/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 
 class ExpenseForm(forms.Form):
+
     title = forms.CharField()
     amount = forms.IntegerField()
     category = forms.CharField()
--- finance_planning/budget/models.py
+++ finance_planning/budget/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils.text import slugify
 # from month.models import MonthField
@@ -5,6 +6,7 @@ from django.utils.text import slugify
 
 
 class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=100)  # Change to month
     slug = models.SlugField(max_length=100, unique=True, blank=True)
     budget = models.IntegerField()
@@ -21,11 +23,14 @@ class Project(models.Model):
         total_expense_amount = 0
         for expense in expense_list:
             total_expense_amount += expense.amount
-        return self.budget - total_expense_amount
+        return int(self.budget - total_expense_amount)
 
     def total_transactions(self):
         expense_list = Expense.objects.filter(project=self)
-        return len(expense_list)
+        total_expense_amount = 0
+        for expense in expense_list:
+            total_expense_amount += expense.amount
+        return f'{len(expense_list)} -  ${int(total_expense_amount)}'
 
     class Meta:
         verbose_name = 'Project'
