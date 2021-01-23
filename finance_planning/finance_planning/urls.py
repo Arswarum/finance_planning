59ba48501678935def91d6b17875760a9b54d7ed
arswarum <bajda.dmytro@gmail.com>
2021-01-23 20:41:43 +0100

    translations done but not all

--- finance_planning/finance_planning/urls.py
+++ finance_planning/finance_planning/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('admin/', admin.site.urls),
+]
+
+urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('budget.urls')),
     path('accounts/', include('users.urls'))
-]
+)
